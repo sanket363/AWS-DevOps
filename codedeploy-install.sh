6cee5b6d00cd40d443007e41516a60f87ef614ad
sanket363 <98816965+sanket363@users.noreply.github.com>
2023-03-31 20:34:07 +0530

    Update codedeploy-install.sh

--- codedeploy-install.sh
+++ codedeploy-install.sh
@@ -3,7 +3,7 @@
 sudo apt-get update 
 sudo apt-get install ruby-full ruby-webrick wget -y 
 cd /tmp 
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/releases/codedeploy-agent_1.3.2-1902_all.deb 
+wget https://aws-codedeploy-write-your-region-name.s3.write-your-region-name.amazonaws.com/releases/codedeploy-agent_1.3.2-1902_all.deb 
 mkdir codedeploy-agent_1.3.2-1902_ubuntu22 
 dpkg-deb -R codedeploy-agent_1.3.2-1902_all.deb codedeploy-agent_1.3.2-1902_ubuntu22 
 sed 's/Depends:.*/Depends:ruby3.0/' -i ./codedeploy-agent_1.3.2-1902_ubuntu22/DEBIAN/control 
